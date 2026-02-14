ee2819bb1247ac99c30249da5625fb205c367930
Matt Russell <mgrbyte@member.fsf.org>
2026-02-14 15:07:44 +0000

    Fix zsh PROMPT_EOL_MARK

--- home/shell.nix
+++ home/shell.nix
@@ -64,6 +64,9 @@
       # Don't suggest commands that start with space (security: space-prefixed commands are private)
       ZSH_AUTOSUGGEST_HISTORY_IGNORE="( *)"
 
+      # Disable partial line marker (the % shown when output doesn't end with newline)
+      PROMPT_EOL_MARK=''
+
       # Oh-my-posh prompt
       eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config.json)"
 
